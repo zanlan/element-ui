6fc0d8a9327a7c89017a468405d06e711c43f6c3
zhangliang <htlzl123@163.com>
2020-11-12 17:47:39 +0800

    2020/11/12 17:46

---
+++ src/views/Collapse.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <!-- <zl-table :datas="datas1" key="1"></zl-table>
+    <zl-table :datas="datas2" key="2"></zl-table>
+    <zl-table :datas="datas3" key="3"></zl-table>
+    <zl-table :datas="datas4" key="4"></zl-table>
+    <zl-table :datas="datas5" key="5"></zl-table> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
+
